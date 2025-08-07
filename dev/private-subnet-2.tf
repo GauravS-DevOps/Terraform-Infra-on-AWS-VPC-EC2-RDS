7b6283aaf34375c3fac72445568ffd8d0677346d
Devopsgauravsingh <gaurav.singh@orangecurrent.com>
2025-08-07 16:01:59 +0530

    all tf files are pushed

--- dev/private-subnet-2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "project_private_subnet_2" {
-  vpc_id            = aws_vpc.project_vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "project_private_subnet_2"
-  }
-}
